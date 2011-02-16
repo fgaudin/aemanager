da71013c6760d28a6e4f4b5420627da6b2a237f8
Francois Gaudin <alf@alfbox.net>
2011-02-17 00:24:54 +0100

    Fix translation

--- project/forms.py
+++ project/forms.py
@@ -36,6 +36,7 @@ class ProposalForm(ModelForm):
         exclude = ['owner', 'project', 'update_date', 'amount']
 
 class ProposalRowForm(ModelForm):
+    quantity = forms.DecimalField(max_digits=5, decimal_places=1, label=_('Quantity'), localize=True)
     unit_price = forms.DecimalField(max_digits=12, decimal_places=2, label=_('Unit price'), localize=True)
 
     class Meta:
