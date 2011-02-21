3fe0f73d9c9ca177cefd61636f10be77aa1261d0
Francois Gaudin <alf@alfbox.net>
2011-02-21 17:25:03 +0100

    Remove bank information from form.

--- autoentrepreneur/forms.py
+++ autoentrepreneur/forms.py
@@ -7,7 +7,6 @@ from autoentrepreneur.models import UserProfile, AUTOENTREPRENEUR_ACTIVITY, \
 class UserProfileForm(ModelForm):
     company_name = forms.CharField(required=False, max_length=255, label=_('Company name'))
     company_id = forms.CharField(max_length=50, label=_('Company id')) # SIRET for France
-    bank_information = forms.CharField(required=False, max_length=255, label=_('Bank information'))
     activity = forms.ChoiceField(choices=AUTOENTREPRENEUR_ACTIVITY, label=_('Activity'))
     creation_date = forms.DateField(label=_('Creation date'))
     creation_help = forms.BooleanField(required=False, label=_('Creation help')) # accre
