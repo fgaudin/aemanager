b93d39ea9b0df551164a104f37ae61f111d57434
Francois Gaudin <alf@alfbox.net>
2011-03-02 02:23:03 +0100

    Added help text on date field on settings. Closes #135.

--- autoentrepreneur/forms.py
+++ autoentrepreneur/forms.py
@@ -8,7 +8,7 @@ class UserProfileForm(ModelForm):
     company_name = forms.CharField(required=False, max_length=255, label=_('Company name'))
     company_id = forms.CharField(max_length=50, label=_('Company id')) # SIRET for France
     activity = forms.ChoiceField(choices=AUTOENTREPRENEUR_ACTIVITY, label=_('Activity'))
-    creation_date = forms.DateField(label=_('Creation date'))
+    creation_date = forms.DateField(label=_('Creation date'), help_text=_('format: mm/dd/yyyy'))
     creation_help = forms.BooleanField(required=False, label=_('Creation help')) # accre
     freeing_tax_payment = forms.BooleanField(required=False, label=_('Freeing tax payment')) # versement liberatoire
     payment_option = forms.ChoiceField(choices=AUTOENTREPRENEUR_PAYMENT_OPTION, label=_('Payment option'))
