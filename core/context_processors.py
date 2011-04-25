78418d872e98dd56776f0a91349f222faf78d4dd
Francois Gaudin <alf@alfbox.net>
2011-04-26 00:57:39 +0200

    Updated version for next release.

--- core/context_processors.py
+++ core/context_processors.py
@@ -5,7 +5,7 @@ def parent_site(request):
             'parent_site_url': settings.PARENT_SITE_URL}
 
 def version(request=None):
-    return {'version': '1.1'}
+    return {'version': '1.1.1'}
 
 def google_api_key(request):
     return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
