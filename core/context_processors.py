f733fe4903329ff177bfb968698e5197cb4249cb
Francois Gaudin <alf@alfbox.net>
2011-09-10 23:28:35 +0200

    Updated aemanager version

--- core/context_processors.py
+++ core/context_processors.py
@@ -3,6 +3,6 @@ from django.conf import settings
 def common(request=None):
     return {'logo_url': settings.LOGO_URL,
             'parent_site_url': settings.PARENT_SITE_URL,
-            'version': '1.2',
+            'version': '1.3',
             'GOOGLE_API_KEY': settings.GOOGLE_API_KEY,
             'demo_mode': settings.DEMO}
